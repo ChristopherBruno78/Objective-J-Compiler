828fb762d54e16bc584a3bba913daa385b4607a8
Chris Bruno <cbruno@hokuasoftware.com>
2021-01-17 15:55:44 -0600

    code cleanup

--- ojc.js
+++ ojc.js
@@ -1,75 +1,74 @@
+#!/usr/bin/env node
+
+"use strict";
 
 const AcornObj = require("acorn-objj"),
-CompilerObj = require("./lib/compiler.js"),
-CodeGenerator = require("./lib/code-generator.js"),
-IssueHandler = require("acorn-issue-handler"),
-fs = require("fs"),
-path = require('path');
+    CompilerObj = require("./lib/compiler.js"),
+    CodeGenerator = require("./lib/code-generator.js"),
+    IssueHandler = require("acorn-issue-handler"),
+    fs = require("fs"),
+    path = require('path');
 
 
 function compile(source, sourcePath) {
 
-	//acorn options
-	var options = {
-		sourceType : 'module'
-	};
+    //acorn options
+    const options = {
+        sourceType: 'module'
+    };
 
-	let issues = new IssueHandler.IssueList(),
-		  ast = null;
-	 try {
+    let issues = new IssueHandler.IssueList(),
+        ast = null;
+    try {
+        ast = AcornObj.parse.parse(source, options, issues);
+    } catch (ex) {
+        if (IssueHandler.isIssue(ex)) {
+            printIssue(ex, sourcePath);
+        } else {
+            throw ex;
+        }
+    }
 
-		 ast = AcornObj.parse.parse(source, options, issues);
-
-	 } catch (ex) {
-         if (IssueHandler.isIssue(ex)) {
-			         printIssue(ex, sourcePath);
-         }
-         else {
-             throw ex;
-         }
-	 }
-
-	 if ( ast ) {
-	   var cOptions = {
-	 		      objjScope : false,
-            classDefs : new Map(),
-            protocolDefs : new Map(),
+    if (ast) {
+        const cOptions = {
+            objjScope: false,
+            classDefs: new Map(),
+            protocolDefs: new Map(),
             typedefs: new Map(),
             importedFiles: new Set()
-	 	       };
+        };
 
-	 	 global.DEBUG = false;
+        global.DEBUG = false;
 
-     Object.assign(CompilerObj.defaultOptions, cOptions);
+        Object.assign(CompilerObj.defaultOptions, cOptions);
 
-	   const compiler = new CompilerObj.Compiler(source, sourcePath, ast, issues, CompilerObj.defaultOptions);
-	   compiler.compileWithFormat(CodeGenerator);
+        const compiler = new CompilerObj.Compiler(source, sourcePath, ast, issues, CompilerObj.defaultOptions);
+        compiler.compileWithFormat(CodeGenerator);
 
-		 if (compiler.errorCount > 0) {
-			 let count = compiler.errorCount,
-			 	    i = 0,
-			      errors = false;
-			  for (; i < count; i++) {
-  				 let ex = compiler.issuesList.issues[i];
-  				 if (ex.severity === 'error') {
-  				 	 printIssue(ex, sourcePath);
-  				 	 errors = true;
-  				 }
-			 }
+        if (compiler.errorCount > 0) {
+            let count = compiler.errorCount,
+                i = 0,
+                errors = false;
+            for (; i < count; i++) {
+                let ex = compiler.issuesList.issues[i];
+                if (ex.severity === 'error') {
+                    printIssue(ex, sourcePath);
+                    errors = true;
+                }
+            }
 
-			 if (errors)
-			 	return;
-		 }
-	 	 return compiler.code;
-	 }
+            if (errors)
+                return;
+        }
+        return compiler.code;
+    }
 
-	 return;
-};
+
+}
 
 function printIssue(ex, sourcePath) {
 
     let buf = [];
-
     if (ex) {
         buf.push(ex.name + ':');
         if (ex.message) {
@@ -87,16 +86,16 @@ function printIssue(ex, sourcePath) {
 const fileNames = process.argv.slice(2);
 
 let out = "";
-for(var i in fileNames) {
-   const pathName = path.resolve(fileNames[i]);
-   try {
-      let source = fs.readFileSync(pathName, 'utf8');
-      let code = compile(source, pathName);
-      out += code;
-   }catch(e) {
-      console.log(e);
-      console.error(`File not found: ${pathName}`);
-   }
+for (var i in fileNames) {
+    const pathName = path.resolve(fileNames[i]);
+    try {
+        let source = fs.readFileSync(pathName, 'utf8');
+        let code = compile(source, pathName);
+        out += code;
+    } catch (e) {
+        console.log(e);
+        console.error(`File not found: ${pathName}`);
+    }
 }
 
 console.log(out);
